86ae446303a7a4a63e1cbde9540f024fde98b5f2
ROTY Jeremy <rotyjeremy@gmail.com>
2024-03-11 15:48:31 +0100

    Adding libs and css

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">    	
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="public/libs/material-design-lite/material.min.css">
+    <link rel="stylesheet" href="public/css/default.css">
+    <script defer src="public/libs/material-design-lite/material.min.js"></script>
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
